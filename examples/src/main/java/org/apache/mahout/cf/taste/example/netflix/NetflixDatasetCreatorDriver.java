b21165e3f39622ba17d5a4a55cfd10044aa4d6b0
Sisir Koppaka <sisir.koppaka@gmail.com>
2010-06-19 17:19:47 +0530

    minor

--- examples/src/main/java/org/apache/mahout/cf/taste/example/netflix/NetflixDatasetCreatorDriver.java
+++ examples/src/main/java/org/apache/mahout/cf/taste/example/netflix/NetflixDatasetCreatorDriver.java
@@ -17,33 +17,12 @@
 
 package org.apache.mahout.cf.taste.example.netflix;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.apache.commons.cli2.CommandLine;
-import org.apache.commons.cli2.Group;
-import org.apache.commons.cli2.Option;
-import org.apache.commons.cli2.OptionException;
 import org.apache.commons.cli2.builder.ArgumentBuilder;
 import org.apache.commons.cli2.builder.DefaultOptionBuilder;
 import org.apache.commons.cli2.builder.GroupBuilder;
-import org.apache.commons.cli2.commandline.Parser;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.DefaultStringifier;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.util.GenericsUtil;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.mahout.analysis.WikipediaAnalyzer;
-import org.apache.mahout.common.CommandLineUtil;
-import org.apache.mahout.common.FileLineIterable;
-import org.apache.mahout.common.HadoopUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,25 +30,23 @@ import org.slf4j.LoggerFactory;
  * Create and run the Netflix Dataset Creator.
  */
 public final class NetflixDatasetCreatorDriver {
-  private static final Logger log = LoggerFactory.getLogger(NetflixDatasetCreatorDriver.class);
+  private static final Logger log = LoggerFactory
+      .getLogger(NetflixDatasetCreatorDriver.class);
   
-  private NetflixDatasetCreatorDriver() { }
+  private NetflixDatasetCreatorDriver() {}
   
   public static void main(String[] args) throws IOException {
     DefaultOptionBuilder obuilder = new DefaultOptionBuilder();
     ArgumentBuilder abuilder = new ArgumentBuilder();
     GroupBuilder gbuilder = new GroupBuilder();
     
-    
   }
   
-
   public static void runJob() throws IOException {
     JobClient client = new JobClient();
     JobConf conf = new JobConf(NetflixDatasetCreatorDriver.class);
-    if (NetflixDatasetCreatorDriver.log.isInfoEnabled()) {
-    }
-   //fill in
+    if (NetflixDatasetCreatorDriver.log.isInfoEnabled()) {}
+    // fill in
     JobClient.runJob(conf);
   }
 }
